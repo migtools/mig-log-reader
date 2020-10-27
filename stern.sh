37f25762a8d935778a91dea355ead62f93dce97b
Derek Whatley <derekwhatley@gmail.com>
2020-10-27 18:28:07 -0400

    Remove registry from log tail; too noisy

--- stern.sh
+++ stern.sh
@@ -1,11 +1,9 @@
 CONFIGPATH=/var/cache/sa2kubeconfig/kubeconfig-${KSUFFIX}
 
-stern "migration-controller|migration-ui|registry|restic|velero" \
+stern "migration-controller|migration-ui|restic|velero" \
 --color ${STERN_COLOR} \
 --exclude-container discovery \
 --exclude "watch is too old"  \
---exclude "level=debug msg=.*s3aws.Stat" \
 --exclude "Found new dockercfg secret" \
---exclude "Ignoring unrecognized environment variable REGISTRY" \
 --since 5s \
 --kubeconfig ${CONFIGPATH} \
