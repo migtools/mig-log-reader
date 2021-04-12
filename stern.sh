cf8588f5609727bea7f2acf888c2e0b269527d06
Derek Whatley <dwhatley@redhat.com>
2021-04-12 13:08:34 -0400

    Update stern.sh

--- stern.sh
+++ stern.sh
@@ -6,6 +6,11 @@ stern -l app.kubernetes.io/part-of=openshift-migration \
 --exclude-container discovery \
 --exclude "watch is too old"  \
 --exclude "Found new dockercfg secret" \
+--exclude "backup location named \\\"default\\\" was not found" \
+--exclude "needs to be at least 1 backup location" \
+--exclude "No backup locations were ready to be verified" \
+--exclude "Backup cannot be garbage-collected" \
+--exclude "Error checking repository for stale locks" \
 --since 5s \
 --all-namespaces \
 --kubeconfig ${CONFIGPATH} \
